ecde85bd421b42b6ff1ced59f54d28f7fba9568e
EugeBr <eugebordenave@gmail.com>
2022-07-19 15:58:36 +0200

    Market model

---
+++ server/marketsData.js
@@ -0,0 +1,41 @@
+const markets = [
+    {
+        name:"Victor Hugo Market",
+        imageUrl: "",
+        location: {
+            city: "Tolouse",
+            country: "France",
+            coordinates:[],
+        },
+        type: "Fresh Food market",
+        Description: "One of our favorite food markets in Europe is the Marché Victor Hugo in Toulouse.  In addition to the usual fare (huge coils of local sausage, delicious breads, and decadent cakes), there is also a restaurant hall upstairs.  It seems that most locals while away a Sunday afternoon here lingering over a delicious three course menu (with wine of course!).",
+        opening_days: ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+        website: "https://www.marche-victor-hugo.fr/"
+    },
+    {
+        name:"La Boqueria",
+        imageUrl: "",
+        location: {
+            city: "Barcelona",
+            country: "Spain",
+            coordinates:[],
+        },
+        type: "Fresh Food market",
+        Description: "Located just off Las Ramblas, La Boqueria in Barcelona is a riot of colors and sound.  Fresh produce, juices, and sweets are of course on offer.  I remember also being jarred when I suddenly rounded a corner and stood face to face (literally) with a skinned goat's head.  You can find anything and everything in this large covered market hall.",
+        opening_days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+        website: "http://www.boqueria.barcelona/home"
+    },
+    {
+        name:"",
+        imageUrl: "",
+        location: {
+            city: "",
+            country: "",
+            coordinates:[],
+        },
+        type: "",
+        Description: "",
+        opening_days: [],
+        website: ""
+    },
+]
---
+++ server/models/Market.model.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose")
+
+const marketSchema = new Schema(
+    {
+        name: {
+            type: String,
+            require: true
+        },
+        imageUrl: String,
+        location: {
+            city: String,
+            country: String,
+            type: { type: String },
+            coordinates: [Number]
+        },
+        type: {
+            type: String,
+            enum: ["Farmers market", "Fresh Food market", "Flea market", "Street Food market"]
+        },
+        Description: String,
+        opening_days: {
+            type: String,
+            enum: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        },
+        website: String,
+        stars: Number,
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+        reviews: [{
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }]
+    }
+)
+
+const Market = model("Market", marketSchema)
+
+module.exports = Market
